569b0c139450d435fd449465e5f59aba73ea143a
Artem <artem.antonov1993@gmail.com>
2020-03-04 22:44:49 +0300

    feat: add solution of the task 'Brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map((item) => item.join(""));
+    let strTest = str;
+    for (let i = 0; i < brackets.length; i++) {
+        if (strTest.indexOf(brackets[i]) != -1) {
+            strTest = strTest.replace(brackets[i], '');
+            i = -1;
+        }
+    }
+    return strTest.length === 0 ? true : false;
 }
